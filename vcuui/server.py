2008a606556f3561f916180d7e34778d79f37454
Rene Straub <rene@see5.ch>
2019-12-28 12:58:38 +0100

    Use dedicated bottle application

--- vcuui/server.py
+++ vcuui/server.py
@@ -7,7 +7,7 @@ Uses bootle webserver in single thread mode
 import os
 
 import bottle
-from bottle import post, request, route, run, static_file
+from bottle import Bottle, post, request, route, run, static_file
 
 from vcuui._version import __version__ as version
 from vcuui.pageinfo import render_page
@@ -22,18 +22,19 @@ module_path = os.path.dirname(path)
 print(f'Running server from {module_path}')
 bottle.TEMPLATE_PATH.insert(0, module_path)
 
-# app = xxxx
+app = Bottle()
+bottle.debug(True)
 
 
 # Static CSS Files
 # @route('/static/css/<filename:re:.*\.css>')
-@route(r'<filename:re:.*\.css>')
+@app.route(r'<filename:re:.*\.css>')
 def send_css(filename):
     return static_file(filename, root=module_path)
 
 
 # Action handler
-@post('/action')
+@app.post('/action')
 def action():
     method = request.forms.get('method')
     print(f'method {method} selected')
@@ -58,15 +59,15 @@ def action():
 
 
 # Mainpage
-@route('/')
-@route('/info')
+@app.route('/')
+@app.route('/info')
 def info():
     return render_page()
 
 
 def run_server(port=80):
     # run(host='0.0.0.0', port=port, debug=True, reloader=True)
-    run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
 
 
 # Can be invoked with python3 -m vcuui.ui
