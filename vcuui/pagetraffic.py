b469fa56c5d48a8f299fba8d6c674523f45e92c9
Rene Straub <rene@see5.ch>
2021-05-14 20:41:01 +0200

    UI improvements
    - Info text about 5 minute update rate
    - Set decimal digits

--- vcuui/pagetraffic.py
+++ vcuui/pagetraffic.py
@@ -38,15 +38,15 @@ class TrafficHandler(tornado.web.RequestHandler):
 
                 rx = int(info['day_rx']) / 2**20
                 tx = int(info['day_tx']) / 2**20
-                tes.append(TE('Day', f'Rx: {rx:.1f} MiB<br>Tx: {tx:.2f} MiB'))
+                tes.append(TE('Day', f'Rx: {rx:.2f} MiB<br>Tx: {tx:.2f} MiB'))
 
                 rx = int(info['month_rx']) / 2**20
                 tx = int(info['month_tx']) / 2**20
-                tes.append(TE('Month', f'Rx: {rx:.1f} MiB<br>Tx: {tx:.0f} MiB'))
+                tes.append(TE('Month', f'Rx: {rx:.1f} MiB<br>Tx: {tx:.1f} MiB'))
 
                 rx = int(info['year_rx']) / 2**30
                 tx = int(info['year_tx']) / 2**30
-                tes.append(TE('Year', f'Rx: {rx:.1f} GiB<br>Tx: {tx:.2f} GiB'))
+                tes.append(TE('Year', f'Rx: {rx:.2f} GiB<br>Tx: {tx:.2f} GiB'))
             else:
                 data['traffic-wwan0'] = 'false'
 
