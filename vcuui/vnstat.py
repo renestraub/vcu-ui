b6f20dc462e6ca3b2f0f7de0cd10cb5071c243d7
Rene Straub <rene@see5.ch>
2022-12-04 19:32:54 +0100

    Improve vnstat error handling

--- vcuui/vnstat.py
+++ vcuui/vnstat.py
@@ -27,27 +27,30 @@ class VnStat:
         logger.debug(f'getting traffic for {self.__if}')
 
         try:
+            # Run vnstat to get traffic information for interface
+            # Parse returned output
+            # 1;wwan0;2022-12-04;34572019;50154378;84726397;1370;2022-12;34572019;50154378;84726397;263;34572019;50154378;84726397
             vnstat_call = [VnStat.BIN, '-d', '--oneline', 'b']
             cp = subprocess.run(vnstat_call, capture_output=True)
             if cp.returncode == 0:
                 result = cp.stdout.decode().strip().split(';')
+                if len(result) >= 13 and result[1] == self.__if:
+                    rx_day = result[3]
+                    tx_day = result[4]
+                    rx_month = result[8]
+                    tx_month = result[9]
+                    rx_year = result[12]
+                    tx_year = result[13]
 
-                rx_day = result[3]
-                tx_day = result[4]
-                rx_month = result[8]
-                tx_month = result[9]
-                rx_year = result[12]
-                tx_year = result[13]
-
-                info = {
-                    'day_rx': int(rx_day),
-                    'day_tx': int(tx_day),
-                    'month_rx': int(rx_month),
-                    'month_tx': int(tx_month),
-                    'year_rx': int(rx_year),
-                    'year_tx': int(tx_year)
-                }
-                return info
+                    info = {
+                        'day_rx': int(rx_day),
+                        'day_tx': int(tx_day),
+                        'month_rx': int(rx_month),
+                        'month_tx': int(tx_month),
+                        'year_rx': int(rx_year),
+                        'year_tx': int(tx_year)
+                    }
+                    return info
 
         except FileNotFoundError:
             logger.info(f'{VnStat.BIN} not found')
